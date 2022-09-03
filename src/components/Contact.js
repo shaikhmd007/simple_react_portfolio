6ebee7123c908a9f579ac2615ce21ae3cfab8abe
shaikh_md.007 <94097592+shaikhmd007@users.noreply.github.com>
2022-09-04 01:20:10 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,11 +2,11 @@ const Contact = (_) => {
   return (
     <div>
       <h4> 👇Connect Me</h4>
-      <a href="" target="_blank">Insta</a>
-      <a href="">LinkedIn </a>
-      <a href="">Facebook </a>
-      <a href="">Twitter </a>
-      <a href="">GitHub </a>
+      <a href="#" target="_blank" >Insta</a>
+      <a href="https://www.linkedin.com/in/shaikhmd007">LinkedIn </a>
+      <a href="#">Facebook </a>
+      <a href="#">Twitter </a>
+      <a href="https://github.com/shaikhmd007">GitHub </a>
     </div>
   );
 };
