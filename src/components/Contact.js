6d17d96b41e37cb5af1930f59eff0bbb52d29ab8
shaikh_md.007 <94097592+shaikhmd007@users.noreply.github.com>
2022-09-08 23:09:25 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,18 @@
+
 const Contact = (_) => {
   return (
     <div>
       <h4> 👇Connect Me</h4>
-      <a href="https://www.instagram.com/shaikh_md.007/?hl=en" target="_blank">Insta</a>
-      <a href="https://www.linkedin.com/in/shaikhmd007" target="_blank">LinkedIn </a>
-      <a href="https://www.facebook.com/profile.php?id=100078348746885" target="_blank">Facebook </a>
-      <a href="#" target="_blank">Twitter </a>
-      <a href="https://github.com/shaikhmd007" target="_blank">GitHub </a>
+      <a href="https://www.instagram.com/shaikh_md.007" Using target="_blank" without rel="noreferrer" >Insta</a>
+      <a href="https://www.linkedin.com/in/shaikhmd007" Using target="_blank" without rel="noreferrer">LinkedIn </a>
+      <a href="https://www.facebook.com/people/MD-Shaikh/100078348746885/" Using target="_blank" without rel="noreferrer">Facebook </a>
+      <a href="https://twitter.com/shaikhmd007" Using target="_blank" without rel="noreferrer" >Twitter </a>
+    <a href="https://github.com/shaikhmd007" Using target="_blank" without rel="noreferrer" >GitHub </a>
+
     </div>
+
   );
+
 };
+
 export default Contact;
